f980d0e7c366a72815aab2211956b501fa5f3d2f
nguyenyou <tunt081295@gmail.com>
2025-05-11 12:31:42 +0700

    🚢 ship it

--- main.ts
+++ main.ts
@@ -3,8 +3,8 @@ import "./style.css";
 // @ts-ignore
 import { Main } from "./out/www/fullLinkJS.dest/main.js";
 
-const mupdfWorkerUrl = new URL("./workers/mupdf.worker", import.meta.url).toString()
-console.log("mupdfjs worker url", mupdfWorkerUrl)
+const mupdfWorkerUrl = new URL("./workers/mupdf.worker", import.meta.url)
+console.log("mupdfjs worker url", mupdfWorkerUrl.toString())
 
 const worker = new Worker(mupdfWorkerUrl, {
   type: "module",
